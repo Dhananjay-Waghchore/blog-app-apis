92a0ca1a7e2269ae86f652edbadaf38777664685
DhananjayWaghchore <dhananjaywaghchore@gmail.com>
2023-02-15 16:31:15 +0530

    Detailed packaging structure

---
+++ Blog-App-Api/src/main/java/com/blogApp/BlogAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.blogApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppApiApplication.class, args);
+	}
+
+}
